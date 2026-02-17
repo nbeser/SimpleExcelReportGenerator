e223de6fc06f417a855d20c4c8869e227e78a823
Nurettin Beser <nurettinbeser@yahoo.com>
2026-02-17 13:06:14 +0300

    output sheets are improved. more features are added

--- src/report.py
+++ src/report.py
@@ -14,9 +14,23 @@ def generate_report(input_path, output_path):
 
     df["Toplam"] = df["Adet"] * df["Birim Fiyat"]
 
+
     total_revenue = df["Toplam"].sum()
-    product_summary = df.groupby("Ürün")["Toplam"].sum().reset_index()
+    total_orders = len(df)
+    total_quantity = df["Adet"].sum()
+    avarage_order_value = df["Toplam"].mean()
+    most_profitable_product = (
+        df.groupby("Ürün")["Toplam"]
+        .sum()
+        .idxmax()   #returns the raw label of max value
+    )
+    # product_summary = df.groupby("Ürün")["Toplam", "Adet"].sum().reset_index()
+    # newer one is:
+    toplam = df.groupby("Ürün")["Toplam"].sum().reset_index()
+    satis = df.groupby("Ürün")["Adet"].sum().reset_index()
+    product_summary = pd.merge(toplam, satis, how="inner")
     
+
     df["Ay"] = df["Tarih"].dt.strftime("%Y-%m")
 
     monthly_summary = (
@@ -25,13 +39,33 @@ def generate_report(input_path, output_path):
         .reset_index()
     )
 
+    total_summary_data = {
+        "Birim": [
+            "Satışların Toplamı",
+            "Sipariş Sayısı",
+            "Toplam Satış Sayısı",
+            "Ortalama Sipariş Değeri",
+            "En Karlı Ürün"
+        ],
+        "Değerler": [
+            total_revenue,
+            total_orders,
+            total_quantity,
+            int(avarage_order_value),
+            most_profitable_product
+        ]
+    }
+    total_summary = pd.DataFrame(total_summary_data)
+
     with pd.ExcelWriter(output_path, engine="openpyxl") as writer:
         df.to_excel(writer, sheet_name="Ham Veri", index=False)
         product_summary.to_excel(writer, sheet_name="Ürün Özeti", index=False)
         monthly_summary.to_excel(writer, sheet_name="Aylık Özet", index=False)
+        total_summary.to_excel(writer, sheet_name="Genel Özet", index=False)
 
-    print("Rapor oluşturuldu.")
-    print("Genel Ciro:", total_revenue)
+    # for CLI usage
+    # print("Rapor oluşturuldu.")
+    # print("Genel Ciro:", total_revenue)
 
 
 if __name__ == "__main__":
